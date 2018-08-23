e0dbbf75f109bc93a2269dd2d7942f794216a50c
bharding2 <b.harding@gmail.com>
2018-08-22 21:38:03 -0700

    page launch js

---
+++ scripts/launch.js
@@ -0,0 +1,41 @@
+let bodyEl = document.getElementsByTagName('body')[0];
+let titleSections = Array.prototype.slice.call(document.getElementsByTagName('section'));
+let titleTargets = [];
+let namesArray = ['NOAH', 'STELLA', 'FIONA'];
+
+let currentPassAttempt = '';
+
+titleSections.forEach((section) => {
+  titleTargets.push(section.textContent);
+
+  section.addEventListener('click', (event) => {
+    let newPassAttempt = currentPassAttempt + section.textContent;
+
+    if (namesArray.includes(newPassAttempt)) {
+      return grantAccess(newPassAttempt);
+    }
+    
+    let firstLetters = [];
+    let newPassKeys = namesArray.map((name) => {
+      firstLetters.push(name[0]);
+      return name.slice(0, newPassAttempt.length);
+    });
+
+    if (newPassKeys.includes(newPassAttempt)) {
+      console.log(newPassAttempt);
+      return currentPassAttempt = newPassAttempt;
+    }
+
+    if (firstLetters.includes(section.textContent) && newPassAttempt[0] !== section.textContent) {
+      console.log(section.textContent);
+      return currentPassAttempt = section.textContent;
+    }
+  });
+});
+
+function grantAccess(name) {
+  console.log(`Access Granted for ${name}`);
+  if (name === 'STELLA') {
+    launchStella(bodyEl, titleSections);
+  }
+}

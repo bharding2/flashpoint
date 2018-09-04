118c74898722f5ef37db374594b2c16079c88c35
bharding2 <b.harding@gmail.com>
2018-09-03 20:35:13 -0700

    noahs entry page

--- scripts/noah.js
+++ scripts/noah.js
@@ -1,5 +1,11 @@
 window.launchNOAH = function(name, titleSections, parentEl) {
   console.log('noah has entered the building');
 
+  titleSections.forEach((ele, idx) => {
+    setTimeout(() => {
+      ele.className = `noah-color-${idx % 2}`;
+    }, idx * 50);
+  });
+
   window.createMessages(name, parentEl);
 };

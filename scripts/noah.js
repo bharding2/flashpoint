8a286a212663eeb7c8e15bf441c41ef9e729055a
bharding2 <b.harding@gmail.com>
2018-09-13 00:27:21 -0700

    expand selected aside

--- scripts/noah.js
+++ scripts/noah.js
@@ -7,5 +7,38 @@ window.launchNOAH = function(name, titleSections, parentEl) {
     }, idx * 50);
   });
 
-  window.createMessages(name, parentEl);
+  let oldContainerEls = parentEl.querySelectorAll('main');
+
+  oldContainerEls.forEach((currentEl) => {
+    parentEl.removeChild(currentEl);
+  });
+
+  let containerEl = document.createElement('main');
+  containerEl.classList.add(`${name}-container`);
+  parentEl.appendChild(containerEl);
+
+  window.createMessages(name, containerEl);
+  createCanvas(name, containerEl);
+
+  let asideEls = document.querySelectorAll('aside');
+
+  asideEls.forEach((asideEl) => {
+    let asideOuterClass = asideEl.className.split(' ')[0];
+
+    asideEl.addEventListener('click', (event) => {
+      asideEls.forEach((eventEl) => {
+        if (eventEl.classList.contains('none')) return eventEl.classList.remove('none');
+        if (eventEl.classList.contains('extended')) return eventEl.classList.remove('extended');
+        if (eventEl.classList.contains(asideOuterClass)) return eventEl.classList.add('extended');
+        eventEl.classList.add('none');
+      });
+    });
+  });
 };
+
+function createCanvas(name, parentEl) {
+  let asideEl = document.createElement('aside');
+  asideEl.classList.add(`${name}-canvas`);
+
+  parentEl.appendChild(asideEl);
+}

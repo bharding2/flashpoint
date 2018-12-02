9087a236ec7ae3185f5b5168449585369624ff01
bharding2 <b.harding@gmail.com>
2018-12-01 23:18:46 -0800

    adding tones to keyboard object

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -11,98 +11,122 @@ window.createCanvas = function (name, parentEl) {
       let keys = {
         'Tab': { // tab
           note: new Audio('./assets/piano/middle_c.mp3'),
+          tone: 'C4',
           color: 'white',
         }, 
         'Digit1': { // 1
           note: new Audio('./assets/piano/mid_c_sharp.mp3'),
+          tone: 'C#4',
           color: 'black',
         },
         'KeyQ': { // q
           note: new Audio('./assets/piano/middle_d.mp3'),
+          tone: 'D4',
           color: 'white',
         },
         'Digit2': { // 2
           note: new Audio('./assets/piano/mid_d_sharp.mp3'),
+          tone: 'D#4',
           color: 'black',
         },
         'KeyW': { // w
           note: new Audio('./assets/piano/middle_e.mp3'),
+          tone: 'E4',
           color: 'white',
         },
         'KeyE': { // e
           note: new Audio('./assets/piano/middle_f.mp3'),
+          tone: 'F4',
           color: 'white',
         },
         'Digit4': { // 4
           note: new Audio('./assets/piano/mid_f_sharp.mp3'),
+          tone: 'F#4',
           color: 'black',
         },
         'KeyR': { // r
           note: new Audio('./assets/piano/middle_g.mp3'),
+          tone: 'G4',
           color: 'white',
         },
         'Digit5': { // 5
           note: new Audio('./assets/piano/mid_g_sharp.mp3'),
+          tone: 'G#4',
           color: 'black',
         },
         'KeyT': { // t
           note: new Audio('./assets/piano/middle_a.mp3'),
+          tone: 'A4',
           color: 'white',
         },
         'Digit6': { // 6
           note: new Audio('./assets/piano/mid_a_sharp.mp3'),
+          tone: 'A#4',
           color: 'black',
         },
         'KeyY': { // y
           note: new Audio('./assets/piano/middle_b.mp3'),
+          tone: 'B4',
           color: 'white',
         },
         'KeyU': { // u
           note: new Audio('./assets/piano/high_c.mp3'),
+          tone: 'C3',
           color: 'white',
         },
         'Digit8': { // 8
           note: new Audio('./assets/piano/high_c_sharp.mp3'),
+          tone: 'C#3',
           color: 'black',
         },
         'KeyI': { // i
           note: new Audio('./assets/piano/high_d.mp3'),
+          tone: 'D3',
           color: 'white',
         },
         'Digit9': { // 9
           note: new Audio('./assets/piano/high_d_sharp.mp3'),
+          tone: 'D#3',
           color: 'black',
         },
         'KeyO': { // o
           note: new Audio('./assets/piano/high_e.mp3'),
+          tone: 'E3',
           color: 'white',
         },
         'KeyP': { // p
           note: new Audio('./assets/piano/high_f.mp3'),
+          tone: 'F3',
           color: 'white',
         },
         'Minus': { // -
           note: new Audio('./assets/piano/high_f_sharp.mp3'),
+          tone: 'F3',
           color: 'black',
         },
         'BracketLeft': { // [
           note: new Audio('./assets/piano/high_g.mp3'),
+          tone: 'G3',
           color: 'white',
         },
         'Equal': { // =
           note: new Audio('./assets/piano/high_g_sharp.mp3'),
+          tone: 'G#3',
           color: 'black',
         },
         'BracketRight': { // ]
           note: new Audio('./assets/piano/high_a.mp3'),
+          tone: 'A3',
           color: 'white',
         },
         'Backspace': { // delete (backspace)
           note: new Audio('./assets/piano/high_a_sharp.mp3'),
+          tone: 'A#3',
           color: 'black',
         },
         'Backslash': { // \
           note: new Audio('./assets/piano/high_b.mp3'),
+          tone: 'B3',
           color: 'white',
         }
       };
